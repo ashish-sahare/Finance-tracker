1093a5a9aa620d685f95b375fa86f7dd2a0208a2
ashish-sahare <ashishsahare2000@gmail.com>
2022-12-28 23:18:12 +0530

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :installs
   devise_for :users
   devise_scope :user do 
     get '/users/sign_out',to: "devise/sessions#destroy"
   end
- devise_for :installs
-  get 'welcomes/index'
 
+  get 'my_portfolio',to: 'users#my_portfolio'
+  get 'search_stock',to: 'stocks#search'
+
+  get 'welcomes/index'
   root to: "welcomes#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
